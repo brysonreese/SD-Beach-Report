ff9d94a5c87db79f1d8fb07c196b3b5d7b73981e
Bryson Reese <bryson@brysonreese.com>
2026-05-27 08:13:31 -0700

    Initial Commit

---
+++ BeachTime/BeachTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeachTimeApp.swift
+//  BeachTime
+//
+//  Created by Bryson Reese on 5/27/26.
+//
+
+import SwiftUI
+
+@main
+struct BeachTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
